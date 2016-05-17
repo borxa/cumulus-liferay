732bb01d08160e5baa3f870e7a062dcce3a56da0
borxa <borxa@www.a-revolta.es>
2016-05-17 20:23:17 +0200

    Null control

--- src/main/java/es/revoltadosdices/cumulus/util/CumulusUtil.java
+++ src/main/java/es/revoltadosdices/cumulus/util/CumulusUtil.java
@@ -6,6 +6,7 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -15,28 +16,31 @@ import java.util.Map;
  * @author Borxa Varela
  */
 public class CumulusUtil {
-    
+
     private static final Log LOG = LogFactoryUtil.getLog(CumulusUtil.class);
-    
-    private CumulusUtil() {}
+
+    private CumulusUtil() {
+    }
 
     public static Map<String, String> jsonToMap(String json) {
 
         HashMap<String, String> map = new HashMap();
 
-        try {
-            JSONObject jsonObject = JSONFactoryUtil.createJSONObject(json);
-            Iterator<String> iterator = jsonObject.keys();
-            while (iterator.hasNext()) {
-                String key = iterator.next();
-                String value = jsonObject.getString(key);
-                if (value.contains("{")) {
-                    value = StringPool.BLANK;
+        if (Validator.isNotNull(json)) {
+            try {
+                JSONObject jsonObject = JSONFactoryUtil.createJSONObject(json);
+                Iterator<String> iterator = jsonObject.keys();
+                while (iterator.hasNext()) {
+                    String key = iterator.next();
+                    String value = jsonObject.getString(key);
+                    if (value.contains("{")) {
+                        value = StringPool.BLANK;
+                    }
+                    map.put(key, value);
                 }
-                map.put(key, value);
+            } catch (JSONException ex) {
+                LOG.error(ex);
             }
-        } catch (JSONException | NullPointerException ex) {
-            LOG.error(ex);
         }
 
         return map;
